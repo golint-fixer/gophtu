f60916b4241ec6cb4d65e18dfba493f49efc7bcd
Paweł Błaszczyk <blaszczykpb@gmail.com>
2014-07-18 23:17:05 +0200

    Drop format dependency in assert's tests.

--- check_test.go
+++ check_test.go
@@ -2,6 +2,7 @@ package gophtu
 
 import (
 	"fmt"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -13,47 +14,57 @@ func mock(sl *string) func(s string, i ...interface{}) {
 }
 
 func runAssert(f func(func(string, ...interface{}), bool, arg),
-	f2 func(*string) func(string, ...interface{}), s *string, b bool, a arg) {
+	f2 func(*string) func(string, ...interface{}), s *string, b bool, a arg) int {
 	f(f2(s), b, a)
+	return lineNo()
+}
+
+func lineNo() int {
+	_, _, line, ok := runtime.Caller(2)
+	if !ok {
+		line = 1
+	}
+	return line
 }
 
 func Test_assert(t *testing.T) {
-	// TODO: drop format dependency
 	cfg := []struct {
 		err string
 		c   bool
 		a   arg
 	}{
 		{"", true, arg{ExpRes: true}},
-		{"check_test.go:51: \"want: '2', got: '3'\"", false,
+		{"check_test.go:%d: \"want: '2', got: '3'\"", false,
 			arg{ExpRes: true, Expected: 2, Got: 3}},
-		{"check_test.go:51: \"want: '3', got: '4', err: 'ziemniak'\"", false,
+		{"check_test.go:%d: \"want: '3', got: '4', err: 'ziemniak'\"", false,
 			arg{ExpRes: true, Expected: 3, Got: 4, Msg: "ziemniak"}},
-		{"check_test.go:51: \"want: '3', got: '4', Ind: 2 4, err: 'ziemniak'\"",
+		{"check_test.go:%d: \"want: '3', got: '4', Ind: 2 4, err: 'ziemniak'\"",
 			false, arg{ExpRes: true, Expected: 3, Got: 4,
 				Msg: "ziemniak", Ind: []int{2, 4}}},
-		{"check_test.go:51: \"want: '3', got: '4', Ind: 3 5\"", false,
+		{"check_test.go:%d: \"want: '3', got: '4', Ind: 3 5\"", false,
 			arg{ExpRes: true, Expected: 3, Got: 4, Ind: []int{3, 5}}},
-		{"check_test.go:51: \"want: result different than '5', Ind: 2 3\"", true,
+		{"check_test.go:%d: \"want: result different than '5', Ind: 2 3\"", true,
 			arg{ExpRes: false, Expected: 5, Ind: []int{2, 3}}},
-		{"check_test.go:51: \"want: result different than '5'\"", true,
+		{"check_test.go:%d: \"want: result different than '5'\"", true,
 			arg{ExpRes: false, Expected: 5}},
-		{"check_test.go:51: \"want: result different than '5', err: " +
+		{"check_test.go:%d: \"want: result different than '5', err: " +
 			"'I am the error'\"", true, arg{ExpRes: false, Expected: 5,
 			Msg: "I am the error"}},
-		{"check_test.go:51: \"want: result different than '5', Ind: 2 3 4, " +
+		{"check_test.go:%d: \"want: result different than '5', Ind: 2 3 4, " +
 			"err: 'mSg a'\"", true, arg{ExpRes: false, Expected: 5,
 			Msg: "mSg a", Ind: []int{2, 3, 4}}},
 	}
 
 	for i, cfg := range cfg {
 		var s string
-		runAssert(assert, mock, &s, cfg.c, cfg.a)
+		n := runAssert(assert, mock, &s, cfg.c, cfg.a)
 		st := strings.TrimLeft(s, "\b")
-		if cfg.err != st {
-			func() {
-				t.Errorf("expected '%v' equal '%v' (%d)", cfg.err, st, i)
-			}()
+		err := cfg.err
+		if err != "" {
+			err = fmt.Sprintf(err, n)
+		}
+		if err != st {
+			t.Errorf("expected '%v' equal '%v' (%d)", err, st, i)
 		}
 	}
 }
