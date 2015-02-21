b36d8b77b736874a8a4f9608ca1c59a11bca1fcb
Paweł Błaszczyk <blaszczykpb@gmail.com>
2015-02-21 09:56:01 +0100

    ci: Update.

--- times/timeout.go
+++ times/timeout.go
@@ -30,47 +30,59 @@ func init() {
 	}
 }
 
-func process(env string) error {
+var suf2unit = map[string]time.Duration{
+	"ms": time.Millisecond,
+	"s":  time.Second,
+	"m":  time.Minute,
+	"h":  time.Hour,
+}
+
+func procvar(s string) error {
+	t := strings.Split(s, "=")
+	if len(t) != 2 {
+		return errors.New("gophtu: invalid timeout setting: " + s)
+	}
+	r, err := regexp.Compile(t[0])
+	if err != nil {
+		return errors.New("gophtu: invalid timeout regex: " + t[0] +
+			", err: " + err.Error())
+	}
+	m := timRe.FindStringSubmatch(t[1])
+	if len(m) != 4 {
+		return errors.New("gophtu: invalid timeout setting: " + t[1])
+	}
+	u, err := strconv.ParseUint(m[2], 10, 64)
+	if err != nil {
+		return errors.New("gophtu: " + err.Error())
+	}
+	if (m[1] == "*" && m[3] != "") || (m[1] != "*" && m[3] == "") {
+		return errors.New("gophtu: invalid timeout setting: " + t[1])
+	}
+	timeouts[r] = getimeout(m, time.Duration(u))
+	return nil
+}
+
+func getimeout(m []string, u time.Duration) time.Duration {
+	switch m[1] {
+	case "*":
+		return defaultT * time.Duration(u)
+	case "+":
+		return defaultT + time.Duration(u)*suf2unit[m[3]]
+	default:
+		return time.Duration(u) * suf2unit[m[3]]
+	}
+}
+
+func process(env string) (err error) {
 	if env == "" {
-		return nil
+		return
 	}
 	for _, s := range strings.Split(env, sep) {
-		t := strings.Split(s, "=")
-		if len(t) != 2 {
-			return errors.New("gophtu: invalid timeout setting: " + s)
-		}
-		r, err := regexp.Compile(t[0])
-		if err != nil {
-			return errors.New("gophtu: invalid timeout regex: " + t[0] +
-				", err: " + err.Error())
-		}
-		m := timRe.FindStringSubmatch(t[1])
-		if len(m) != 4 {
-			return errors.New("gophtu: invalid timeout setting: " + t[1])
-		}
-		u, err := strconv.ParseUint(m[2], 10, 64)
-		if err != nil {
-			return errors.New("gophtu: " + err.Error())
-		}
-		if (m[1] == "*" && m[3] != "") || (m[1] != "*" && m[3] == "") {
-			return errors.New("gophtu: invalid timeout setting: " + t[1])
-		}
-		c := map[string]time.Duration{
-			"ms": time.Millisecond,
-			"s":  time.Second,
-			"m":  time.Minute,
-			"h":  time.Hour,
-		}
-		switch m[1] {
-		case "*":
-			timeouts[r] = defaultT * time.Duration(u)
-		case "+":
-			timeouts[r] = defaultT + time.Duration(u)*c[m[3]]
-		default:
-			timeouts[r] = time.Duration(u) * c[m[3]]
+		if err = procvar(s); err != nil {
+			return
 		}
 	}
-	return nil
+	return
 }
 
 // Timeout returns default timeout, if one is not explicitly configured
